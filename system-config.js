17b8c86f0541155f37a2a7b401d8c4efe1f92601
Robin Buckley <robin.buckley@auspost.com.au>
2016-06-27 14:16:34 +1000

    Recommit system config...for some reason

---
+++ system-config.js
@@ -0,0 +1,85 @@
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    '@angular2-material': 'vendor/@angular2-material',
+    'watermarkjs': 'vendor/watermarkjs/dist/watermark.js'
+};
+/** User packages configuration. */
+var packages = {
+    '@angular2-material/core': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'core.js'
+    },
+    '@angular2-material/button': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'button.js'
+    },
+    '@angular2-material/card': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'card.js'
+    },
+    '@angular2-material/icon': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'icon.js'
+    },
+    '@angular2-material/progress-circle': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'progress-circle.js'
+    },
+    '@angular2-material/sidenav': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'sidenav.js'
+    },
+    '@angular2-material/toolbar': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'toolbar.js'
+    },
+    'watermarkjs': {
+        format: 'cjs'
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    //'watermarkjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js',
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
